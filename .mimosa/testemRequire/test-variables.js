3a87345cc8c5592d9fddef9d0e2c89f1c4595bd7
blueshirts <milanoa@yahoo.com>
2014-12-06 10:42:31 -0500

    Initial commit.

---
+++ .mimosa/testemRequire/test-variables.js
@@ -0,0 +1,16 @@
+window.MIMOSA_TEST_REQUIRE_CONFIG = {
+  "baseUrl": "/js",
+  "paths": {
+    "jquery": "vendor/jquery/jquery",
+    "lib": "libname"
+  }
+};
+window.MIMOSA_TEST_MOCHA_SETUP = {
+  "ui": "bdd",
+  "globals": [
+    "$"
+  ]
+};
+window.MIMOSA_TEST_SPECS = [
+  "spec/widget_spec"
+];
